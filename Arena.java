4d1d106f75dafd7df7ca895f3446f4ad8a4b3184
Pediredla Manohar <manohar20century@gmail.com>
2024-11-21 11:26:27 +0530

    Checking players health while starting the game

--- Arena.java
+++ Arena.java
@@ -4,6 +4,7 @@ public class Arena {
     private int attackerIndex;
     private int defenderIndex;
 
+    // constructor
     public Arena(Player player1,Player player2){
         this.players = new Player[]{player1,player2};
         this.dice = new Dice();
@@ -11,8 +12,16 @@ public class Arena {
         this.defenderIndex = 0;
     }
 
+    // check player health to continue game to next round
+    public boolean GameOver(){
+        return players[0].getPlayerHealth()<=0 || players[1].getPlayerHealth()<=0;
+    }
+
+    // start the game
     public void playGame(){
         System.out.println("\n--- The Battle Begins! ---");
-        
+        while (!GameOver()) {
+            System.out.println("Continue battle");
+        }
     }
 }
